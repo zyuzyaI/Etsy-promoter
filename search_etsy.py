a32ebb53a533d6ed436369eb6e20dbb83a08a36a
zyuzyaI <46805717+zyuzyaI@users.noreply.github.com>
2019-10-18 09:18:28 +0300

    moduls

---
+++ search_etsy.py
@@ -0,0 +1,10 @@
+# for change IP
+from fake_useragent import UserAgent
+import socks
+import socket
+from bs4 import BeautifulSoup
+#for get html
+import requests
+import time
+#save in csv
+import csv
